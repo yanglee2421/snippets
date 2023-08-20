61abf201cda00035bc3310736016626f7c59ae3c
yanglee <yanglee2421@gmail.com>
2023-08-20 11:05:09 +0800

    Symbol.toPrimitive

--- src/ecma/es-equal.ts
+++ src/ecma/es-equal.ts
@@ -2,11 +2,21 @@
 
 export function esEqual() {
   const symbol = Symbol();
+
   const object = {
     [symbol]: 0,
+
+    // Highest Priority
+    [Symbol.toPrimitive]() {
+      return ++this[symbol];
+    },
+
+    // Takes precedence over toString for anything other than string
     valueOf() {
       return ++this[symbol];
     },
+
+    // Prefer valueOf in the case of strings
     toString() {
       return this[symbol]++;
     },
@@ -17,10 +27,11 @@ export function esEqual() {
 }
 esEqual();
 console.log(void 0 == null);
+console.log(void 0 === null);
 
 /**
  * 1. Special: undefined, null, ,NaN, symbol
  * 2. Same Type: then compare the value
  * 3. Both Are Primitive: convert to number
- * 4. Primitive & Object: convert to primitive by valueOf, toString
+ * 4. Primitive & Object: convert to primitive by Symbol.toPrimitive valueOf, toString
  */
