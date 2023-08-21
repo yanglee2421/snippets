9bc90613c77f52404a820e0fb7c1882cee611262
ZhangSheng <yanglee2421@gmail.com>
2023-08-21 11:32:40 +0800

    sort out code

--- src/ecma/es-deduplicate.ts
+++ src/ecma/es-deduplicate.ts
@@ -1,15 +1,20 @@
 export function toDeduplicate(items: unknown[], ops: Partial<Ops> = {}) {
   const { isCover = false, keyProp = "id" } = ops;
 
+  if (!keyProp) return;
+
   const map = new Map();
 
   items.forEach((item) => {
-    if (!item) throw new Error("invalid item");
-    if (typeof item !== "object") throw new Error("Item must be an object!");
+    // Item must be an object
+    if (typeof item !== "object") return;
+    if (!item) return;
 
+    // Get Key
     const key = Reflect.get(item, keyProp);
-    if (!key) throw new Error("invalid key");
+    if (!key) return;
 
+    // Valid key-value pairs
     if (isCover) return map.set(key, item);
     map.get(key) ?? map.set(key, item);
   });
