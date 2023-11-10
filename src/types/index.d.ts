a1bdec899df74cc12e33f02e72b868742fcaed2f
ZhangSheng <yanglee2421@gmail.com>
2023-11-10 16:56:13 +0800

    Add type ParamType

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -4,3 +4,6 @@ export interface ArrayLike<T> {
   readonly length: number;
   readonly [n: number]: T;
 }
+
+// The infer keyword is used to extract a type from a generic
+type ParamType<TFunc> = TFunc extends (params: infer P) => void ? P : never;
