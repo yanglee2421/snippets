8d9b7eb362b1978a29a2c3fd4cc7f81b3d5dcbd4
yanglee2421 <yanglee2421@gmail.com>
2023-12-10 18:14:14 +0800

    Update readme

---
+++ src/ecma/Promise/retryFetch.ts
@@ -0,0 +1,22 @@
+#! deno run
+
+import { timeout } from "../../common/timeout.ts";
+
+export async function retryFetch(failCount = 3) {
+  try {
+    return await fetch(new URL("/api/joke/list", "https://autumnfish.cn"), {});
+  } catch (error) {
+    console.log(failCount);
+
+    // Retry fetch
+    if (failCount) {
+      await timeout(1000 * 3);
+
+      return await retryFetch(failCount - 1);
+    }
+
+    console.error(error);
+  }
+}
+
+retryFetch();
